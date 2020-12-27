5e740e63f4f288a5b15e2e821484f8650d09f100
Lorenzo Fratus <lorenzo1.fratus@mail.polimi.it>
2020-12-27 23:04:06 +0100

    Basic UI

--- CLup/src/view/js/utils.js
+++ CLup/src/view/js/utils.js
@@ -2,6 +2,28 @@
  * AJAX call management
  */
 
+function submitForm(id = "form") {
+	var form = document.getElementById(id);
+
+	if(!form.checkValidity()) {
+		form.reportValidity();
+		return;
+	}
+
+	var src = form.getAttribute("data-src");
+	var target = form.getAttribute("data-target");
+
+	makeCall("POST", form.getAttribute("data-src"), form, req => {
+		if(req.readyState == XMLHttpRequest.DONE) {
+			if(req.status == 200) {
+				window.location.href = target;
+			} else {
+				M.toast({html: req.responseText, displayLength: (60 * 60 * 1000)});
+			}
+		}
+	});
+}
+
 function makeCall(method, url, formElement, cback, reset = true) {
 	var req = new XMLHttpRequest(); // visible by closure
 	req.onreadystatechange = function() {
