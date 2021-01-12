6783cd012649b523a8c38caa3d36df47b4f6f813
Alessandro <aleferrara1998@gmail.com>
2021-01-12 12:11:33 +0100

    add installation instructions

--- CLup/src/index.js
+++ CLup/src/index.js
@@ -1,5 +1,6 @@
 const server = require('./controller/server')
 
 server.listen(3000, () => {
-    console.log('listening on *:3000')
+    console.log('Server started.')
+    console.log('Listening on http://0.0.0.0:3000')
 })
