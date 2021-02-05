4e31ed8fe5276390fcc7b101838d1026f0b3f4b2
Alessandro <aleferrara1998@gmail.com>
2021-02-05 18:06:08 +0100

    complete clupper route testing

--- CLup/test/ClupperRoute.test.js
+++ CLup/test/ClupperRoute.test.js
@@ -40,31 +40,41 @@ describe("Clupper Route Testing", () => {
     it("Should allow a clupper to get a store info", async () => {
       const storeId = 51760570179
       const res = await agent
-            .get(`http://localhost:${port}/explore/${storeId}`)
+            .get(`http://localhost:${port}/explore/store/`)
+            .query({id: storeId})
             .redirects(0).ok(res => res.status < 400)
 
-      console.log(res)
       expect(res.status).to.be.equal(200)
     })
 
     it("Should not allow a clupper to get a store that doesn't exists", async () => {
       const storeId = 99999999999
       const res = await agent
-            .get(`http://localhost:${port}/explore/${storeId}`)
+            .get(`http://localhost:${port}/explore/store/`)
+            .query({id: storeId})
             .redirects(0).ok(res => res.status < 400)
 
-      expect(tester.compare(res, 302, '/explore')).to.be.true
+      expect(tester.compare(res, 303, '/explore')).to.be.true
     })
 
     it("Should not allow a clupper to get queue status when he's not in queue", async () => {
-
       const res = await agent      
             .get(`http://localhost:${port}/explore/queue/`)
-      expect(tester.compare(res, 302, '/explore')).to.be.true
+            .redirects(0).ok(res => res.status < 400)
+      expect(tester.compare(res, 303, '/explore')).to.be.true
     })
 
-    it("Should allow a clupper to join a queue", async () => {
+    it("Should not allow a clupper to join a non existent queue", async () => {
+      const storeId = 99999999999
+      const res = await agent      
+            .post(`http://localhost:${port}/explore/queue/join`)
+            .type('form')
+            .send({vat: storeId})
+            .redirects(0).ok(res => res.status < 400)
+      expect(tester.compare(res, 303, '/explore')).to.be.true
+  })
 
+    it("Should allow a clupper to join a queue", async () => {
         const res = await agent      
               .post(`http://localhost:${port}/explore/queue/join`)
               .type('form')
@@ -73,18 +83,26 @@ describe("Clupper Route Testing", () => {
     })
 
     it("Should allow a clupper to get queue status", async () => {
-
       const res = await agent      
             .get(`http://localhost:${port}/explore/queue/`)
       expect(res.status).to.be.equal(200)
     })
 
     it("Should allow a clupper to leave a queue", async () => {
-              // Leave queue
-              const res = await agent      
+      const res = await agent      
               .post(`http://localhost:${port}/explore/queue/leave`)
               .type('form')
               .send({vat: 51760570179})
       expect(res.status).to.be.equal(200)
+    })
+
+    it("Should not allow a clupper to leave a not existent queue", async () => {
+      const storeId = 99999999999
+      const res = await agent      
+            .post(`http://localhost:${port}/explore/queue/leave`)
+            .type('form')
+            .send({vat: storeId})
+            .redirects(0).ok(res => res.status < 400)
+      expect(tester.compare(res, 303, '/explore/queue')).to.be.true
     })
   })
